fd6279afc0065cd0889702dcf67052fd4eb33aac
Davvigar <davvigar@gmail.com>
2025-05-02 01:36:03 +0200

    svg flecha, svg camion cambio a rosa

--- html/assets/js/script.js
+++ html/assets/js/script.js
@@ -1,6 +1,28 @@
-const hamMenu = document.querySelector('.ham-menu'); // Selector corregido
-const offScreenMenu = document.querySelector('.off-screen-menu'); // Selector corregido
 
+
+
+
+
+const hamMenu = document.querySelector('.ham-menu'); 
+const offScreenMenu = document.querySelector('.off-screen-menu'); 
+const video = document.querySelector('.hero-video video');
+const heroButton = document.querySelector('.hero-button');
+
+// oculkto el boton de primeras 
+heroButton.style.opacity = '0';
+
+// función para que salga el boton cuando queda tanto tiempo
+
+video.addEventListener('timeupdate', () => {
+    const timeLeft = video.duration - video.currentTime;
+    if (timeLeft <= 7 && timeLeft > 0) {
+        heroButton.style.opacity = '1';
+        heroButton.style.transition = 'opacity 0.5s ease';
+    }
+});
+
+
+// funcion para el burguermenuu
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
