d320326070979721e53349a93e106570e2c45a70
Davvigar <davvigar@gmail.com>
2025-05-02 17:28:17 +0200

    algodon de azucar y patrocionios

--- html/assets/js/script.js
+++ html/assets/js/script.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 const hamMenu = document.querySelector('.ham-menu'); 
 const offScreenMenu = document.querySelector('.off-screen-menu'); 
 const video = document.querySelector('.hero-video video');
@@ -26,4 +21,36 @@ video.addEventListener('timeupdate', () => {
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
+});
+
+// animacion logo
+const leftRow = document.querySelector('.scrolling-row.left .image-row');
+const rightRow = document.querySelector('.scrolling-row.right .image-row');
+
+// Duplico los logos para que no haya esopacios
+const leftImages = leftRow.querySelectorAll('img');
+const rightImages = rightRow.querySelectorAll('img');
+
+leftImages.forEach(img => {
+    const clone = img.cloneNode(true);
+    clone.classList.add('logo-image'); // para que fucncione el hover del color
+    leftRow.appendChild(clone);
+});
+
+rightImages.forEach(img => {
+    const clone = img.cloneNode(true);
+    clone.classList.add('logo-image'); 
+    rightRow.appendChild(clone);
+});
+
+// Aplicar estilos a las imágenes originales también
+leftImages.forEach(img => img.classList.add('logo-image'));
+rightImages.forEach(img => img.classList.add('logo-image'));
+
+window.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY;
+    const scrollSpeed = 0.1;
+    
+    leftRow.style.transform = `translateX(${scrollPosition * scrollSpeed}px)`;
+    rightRow.style.transform = `translateX(${-scrollPosition * scrollSpeed}px)`;
 });
